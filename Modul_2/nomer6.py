1a1c965865a83c47c6d877b1539242db68449b24
L200180150 <44252066+L200180150@users.noreply.github.com>
2020-03-10 22:09:43 +0700

    Add files via upload

---
+++ Modul_2/nomer6.py
@@ -0,0 +1,29 @@
+from manusia import Manusia
+
+class SiswaSMA(Manusia):
+    def __init__(self, nama, kota, jurusan):
+        Manusia.__init__(self, nama)
+        self.kota = kota
+        self.jurusan = jurusan
+
+    def __str__(self):
+        return 'Nama : {} \nJurusan : {} \nKota : {}'.format(self.nama, self.jurusan, self.kota)
+
+    def tampilkanNama(self):
+        print('Nama siswa : {}'.format(self.nama))
+
+    def tampilkanJurusan(self):
+        print('Siswa dengan nama {} memilih jurusan {}'.format(self.nama, self.jurusan))
+
+    def tampilkanKota(self):
+        print('Siswa dengan nama {} tinggal di {}'.format(self.nama, self.kota))
+
+    def makanSiang(self, makanan):
+        print('{} makan {} di kantin dengan keadaan {}'.format(self.nama, makanan, self.keadaan))
+        self.keadaan = 'Laparr'
+
+siswa = SiswaSMA('Prengki', 'Sragen', 'MIPA')
+siswa.tampilkanJurusan()
+siswa.tampilkanKota()
+siswa.olahraga('BolaSepak')
+siswa.makanSiang('Papeda')
