e8eb9d5af9d88d3ffc4f63f7e195ac580cc9cf09
L200180150 <44252066+L200180150@users.noreply.github.com>
2020-03-10 21:29:28 +0700

    Add files via upload

---
+++ Modul_2/nomer4.py
@@ -0,0 +1,8 @@
+from mahasiswa import Mahasiswa
+
+mhs = Mahasiswa('Budi', 12121, 'Denpasar', 213123123)
+
+mhs.ambilKuliah('Algopro')
+mhs.ambilKuliah('Matdis')
+print(mhs.listKuliah)
+
---
+++ Modul_2/nomer7.py
@@ -0,0 +1,23 @@
+from mhstif import MhsTIF
+
+a = MhsTIF('Doni', 213214325, 'Denpasar', 100000)
+
+a.ambilKotaTinggal() #Berasal dari class Mahasiswa
+a.ambilKuliah('Algopro') #Berasal dari class Mahasiswa
+a.ambilNama() #Berasal dari class Mahasiswa
+a.ambilNIM() #Berasal dari class Mahasiswa
+a.ambilUangSaku() #Berasal dari class Mahasiswa
+a.hapusKuliah('Algopro') #Berasal dari class Mahasiswa
+a.katakanPy() #Berasal dari class MhsTIF
+a.keadaan #Berasal dari class Manusia
+a.kota #Berasal dari class Mahasiswa
+a.listKuliah #Berasal dari class Mahasiswa
+a.makan('nasi') #Berasal dari class Manusia
+a.mengalikanDenganDua(8) #Berasal dari class Manusia
+a.nama #Berasal dari class Mahasiswa
+a.NIM #Berasal dari class Mahasiswa
+a.olahraga('Push Up') #Berasal dari class Manusia
+a.perbaruiKotaTinggal('Jakarta') #Berasal dari class Mahasiswa
+a.tambahUangSaku(2131233) #Berasal dari class Mahasiswa
+a.uangSaku #Berasal dari class Mahasiswa
+a.ucapkanSalam() #Berasal dari class Manusia
