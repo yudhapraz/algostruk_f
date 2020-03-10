a5df79d95e0ca01a4ced5ff6419e56716eb324c4
L200180150 <44252066+L200180150@users.noreply.github.com>
2020-03-10 21:34:22 +0700

    Delete nomer5.py

--- Modul_2/nomer5.py
+++
@@ -1,9 +0,0 @@
-from mahasiswa import Mahasiswa
-
-mhs9 = Mahasiswa('Maul', 3234234, 'Cirebon', 20000)
-mhs9.ambilKuliah('Cyber Security')
-mhs9.ambilKuliah('Cryptography')
-mhs9.ambilKuliah('Networking')
-print(mhs9.listKuliah)
-mhs9.hapusKuliah('Networking')
-print(mhs9.listKuliah)
